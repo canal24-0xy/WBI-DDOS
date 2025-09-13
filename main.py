1ba083e5e44c88dde644b66ac0c23dbf5e0f894a
canal24-0xy <canalkun24@gmail.com>
2025-09-13 13:43:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def display_header():
         f"{Fore.CYAN}   ██{Fore.YELLOW}  ║{Fore.CYAN}     ██{Fore.YELLOW}  ║{Fore.CYAN}   ██{Fore.YELLOW}  ║{Fore.CYAN}  ████████████████   ╝    ██{Fore.YELLOW} ║",
         f"{Fore.CYAN}   ██{Fore.YELLOW}  ║{Fore.CYAN}     ██{Fore.YELLOW}  ║{Fore.CYAN}   ██{Fore.YELLOW}  ║{Fore.CYAN}  ██{Fore.YELLOW}  ║{Fore.GREEN}           ██{Fore.YELLOW}  ╗{Fore.GREEN}   ██{Fore.YELLOW}  ║",
         f"{Fore.GREEN}    ██{Fore.YELLOW}  ║{Fore.GREEN}    ██{Fore.YELLOW}  ║{Fore.GREEN}  ██{Fore.YELLOW}  ║{Fore.GREEN}   ██{Fore.YELLOW}  ║{Fore.GREEN}           ██{Fore.YELLOW}  ║{Fore.GREEN}   ██{Fore.YELLOW}  ║",
-        f"{Fore.GREEN}     ██{Fore.YELLOW}  ╚═{Fore.GREEN}█████{Fore.YELLOW}═╝{Fore.GREEN} ██  ║{Fore.GREEN}    ██{Fore.YELLOW}  ║{Fore.GREEN}           ██{Fore.YELLOW}  ║{Fore.GREEN}   ██{Fore.YELLOW}  ║",
+        f"{Fore.GREEN}     ██{Fore.YELLOW}  ╚═{Fore.GREEN}█████{Fore.YELLOW}═╝{Fore.GREEN} ██  ║{Fore.GREEN}    ██{Fore.YELLOW}  ║{Fore.GREEN}           ██{Fore.YELLOW}  ║{Fore.GREEN}  ██{Fore.YELLOW}  ║",
         f"{Fore.GREEN}        ███{Fore.YELLOW}   ║{Fore.GREEN} ███  ║{Fore.GREEN}      ████████████████{Fore.YELLOW}   ║{Fore.GREEN}     ██{Fore.YELLOW} ║",
         f"{Fore.YELLOW}         ╚═══╝   ╚═══╝          ╚════════════╝    ╚═══╝",
         f"{Fore.YELLOW} ",
